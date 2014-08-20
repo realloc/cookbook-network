988c0ba5196c17eb4f5f8364554b704fc93e6e68
Stanislav Bogatyrev <realloc@clodo.ru>
2014-08-20 18:31:26 +0400

    Change metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             "network"
-maintainer       "Clodo.ru"
-maintainer_email "dev@clodo.ru"
+maintainer       "Stanislav Bogatyrev"
+maintainer_email "realloc@realloc.spb.ru"
 license          "Apache 2.0"
 description      "Configures network"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends          "network_interfaces"
-version          "0.0.1"
+version          "0.0.2"
 
